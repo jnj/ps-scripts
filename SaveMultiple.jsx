6631ddde13305b86da0243a6858bfda74c55f880
Josh Joyce <jnj@users.noreply.github.com>
2018-08-19 22:23:34 -0500

    Scripts to save copies of different sizes

---
+++ SaveMultiple.jsx
@@ -0,0 +1,52 @@
+﻿#target photoshop
+#include Utils.jsx
+
+// Saves a resized copy of an image, such that the larger
+// axis (width or height) of the image is maxDimension
+// pixels long.
+function saveAbsResized(doc, maxDimension) {
+    var resampleMethod = ResampleMethod.BICUBICSMOOTHER;
+    var copy = doc.duplicate();
+    
+    if (copy.width > copy.height) {
+        copy.resizeImage(new UnitValue(maxDimension, "px"), null, null, resampleMethod);
+    } else {
+        copy.resizeImage(null, new UnitValue(maxDimension, "px"), null, resampleMethod);
+    }
+    
+    var newName = baseName(doc.name) + "_" + maxDimension + ".jpg";
+    var destPath = new File(doc.path + "/" + newName);
+    var saveOptions = new JPEGSaveOptions();
+    saveOptions.embedColorProfile = true;
+    saveOptions.formatOptions = FormatOptions.OPTIMIZEDBASELINE;
+    saveOptions.quality = 12;
+
+    convertTosRGB(copy);
+    copy.saveAs(destPath, saveOptions, true, Extension.LOWERCASE);
+    copy.close(SaveOptions.DONOTSAVECHANGES);
+}
+
+function savePctResized(doc, pct) {
+    var resampleMethod = ResampleMethod.BICUBICSMOOTHER;
+    var copy = doc.duplicate();
+    var suffix = "";
+    
+    if (pct < 100) {
+        suffix = "R";
+        copy.resizeImage(new UnitValue(pct, "%"), null, null, resampleMethod);
+    }
+    
+    var newName = baseName(doc.name) + "_" + suffix + ".jpg";
+    var destPath = new File(doc.path + "/" + newName);
+    var saveOptions = new JPEGSaveOptions();
+    saveOptions.embedColorProfile = true;
+    saveOptions.formatOptions = FormatOptions.OPTIMIZEDBASELINE;
+    saveOptions.quality = 12;
+
+    convertTosRGB(copy);
+    copy.saveAs(destPath, saveOptions, true, Extension.LOWERCASE);
+    copy.close(SaveOptions.DONOTSAVECHANGES);
+}
+
+saveAbsResized(app.activeDocument, 1080);
+savePctResized(app.activeDocument, 35);
---
+++ Utils.jsx
@@ -0,0 +1,14 @@
+﻿#target photoshop
+
+function baseName(name) {
+    var i = name.lastIndexOf(".");
+    if (i > 0) {
+        return name.substr(0, i);
+    }
+}
+
+function convertTosRGB(doc) {
+    if (doc.colorProfileName.indexOf("sRGB") == -1) {
+        doc.convertProfile("sRGB IEC61966-2.1", Intent.RELATIVECOLORIMETRIC);
+    }
+}
